b317999c6c4f45b162cef95566d756775e20bd3a
StuMartin22 <stuart.martin@rocketmail.com>
2022-04-03 21:15:59 -0500

    beginning to create and modify code to run functions to fetch data from API and convert it.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,59 +1,101 @@
-var citySearch = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}"
+var citySearch = "http://api.openweathermap.org/geo/1.0/direct?q={city name}&limit=5&appid=7395305ec624eec55c8c9f460f751d9d"
 var urlData = document.location.search;
 var searchTerm = $("#searchTerm");
 var submitBtn = $("#submit");
 
 
-
-
-//ALL BELOW IS CODE TO FORMAT TO FETCH API AND ETC
-////////////////////////////////////////////////////////////////
-var searchTerm = $("#search-term");
-var format = $("#format");
-
-submitBtn.on("click", function (event) {
+submitBtn.on("click", function(event) {
     event.preventDefault();
-    // alert("hello");
-    // console.log(searchTerm.val());
-    var userSearchTerm = searchTerm.val();
-    var userFormat = format.val();
-    document.location.href = "./page2.html?search=" + userSearchTerm + "&format=" + userFormat;
+    var city = searchTerm.val();
+//     var newURL = citySearch.split("{city name}").join(city);
+//     // console.log(newURL);
+// fetch(newURL)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         console.log(data.results)
+//     });
+var searchHistory = localStorage.setItem(city);
+console.log(searchHistory);
+var testObject = { 'one': 1, 'two': 2, 'three': 3 };
+
+// Put the object into storage
+localStorage.setItem('cityList', JSON.stringify(cityList));
+
+// Retrieve the object from storage
+var retrievedObject = localStorage.getItem('cityList');
+
+console.log('retrievedObject: ', JSON.parse(retrievedObject));
 });
+var cityList = {}
 
 
-var mainEl = $("#main"); 
-//^ showing results for chicago in a div above a h3 that says that
 
 
-var format = urlData.split("&")[1].split("=")[1];
-var apiUrl = "https://www.loc.gov/collections/?fo=json&title=chicago&original_format=map";
-fetch(apiUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data.results[0]);
-        for (let i = 0; i < data.results.length; i++) {
-            var timeStamp = data.results[i].timestamp;
-            var year = moment(timeStamp).format("YYYY");
-            console.log(year)
-            // console.log(timeStamp)
-            var articleEl = $("<article class='p-10'>");
-            var titleEl = $("<h2>");
-            var pEl = $("<p>");
-            var descriptionEl = $("<p>");
-            var moreBtn = $("<button>");
-            titleEl.text(data.results[i].title);
-            articleEl.append(titleEl);
-            pEl.text(year);
-            articleEl.append(pEl);
-            descriptionEl.text(data.results[i].description[0]);
-            articleEl.append(descriptionEl);
-            // moreBtn.text(data.result[i].url)
-            // articleEl.append(moreBtn);
-            mainEl.append(articleEl);
-        }
-    });
+
+
+
+
+
+
+
+
+
+
+
+
+// //ALL BELOW IS CODE TO FORMAT TO FETCH API AND ETC
+// ////////////////////////////////////////////////////////////////
+// var format = $("#format");
+
+// submitBtn.on("click", function (event) {
+//     event.preventDefault();
+//     // alert("hello");
+//     // console.log(searchTerm.val());
+//     var userSearchTerm = searchTerm.val();
+//     var userFormat = format.val();
+//     document.location.href = "./page2.html?search=" + userSearchTerm + "&format=" + userFormat;
+// });
+
+
+// var mainEl = $("#main"); 
+// //^ showing results for chicago in a div above a h3 that says that
+
+
+// var urlData = document.location.search;
+// var mainEl = $("#main");
+// // console.log(urlData);
+
+// var searchTerm = urlData.split("&")[0].split("=")[1];
+// var format = urlData.split("&")[1].split("=")[1];
+
+// var apiUrl = "https://www.loc.gov/collections/?fo=json&title=chicago&original_format=map";
+
+// fetch(citySearch)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         console.log(data.results[0]);
+//         for (let i = 0; i < data.results.length; i++) {
+
+
+//             var articleEl = $("<article class='p-10'>");
+//             var titleEl = $("<h2>");
+//             var pEl = $("<p>");
+//             var descriptionEl = $("<p>");
+//             var moreBtn = $("<button>");
+
+//             titleEl.text(data.results[i].title);
+//             articleEl.append(titleEl);
+
+//             descriptionEl.text(data.results[i].description[0]);
+//             articleEl.append(descriptionEl);
+
+//             mainEl.append(articleEl);
+//         }
+//     });
     
-////////////////////////////////////////////////////////////////
-//ALL ABOVE IS CODE TO FORMAT FOR SEARCHING API    
+// ////////////////////////////////////////////////////////////////
+// //ALL AB
