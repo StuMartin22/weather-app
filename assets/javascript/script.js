ef04cc8d6a7530314680b1fc32603909bb57456a
StuMartin22 <stuart.martin@rocketmail.com>
2022-04-03 23:39:49 -0500

    app can now properly create URL after putting city name into textbox, stores city name in local storage, and able to receive from local storage as well git status

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,9 +6,8 @@ var submitBtn = $("#submit");
 
 submitBtn.on("click", function(event) {
     event.preventDefault();
-    var city = searchTerm.val();
-//     var newURL = citySearch.split("{city name}").join(city);
-//     // console.log(newURL);
+    var newURL = citySearch.split("{city name}").join(searchTerm.val());
+    console.log(newURL);
 // fetch(newURL)
 //     .then(function (response) {
 //         return response.json();
@@ -16,86 +15,8 @@ submitBtn.on("click", function(event) {
 //     .then(function (data) {
 //         console.log(data.results)
 //     });
-var searchHistory = localStorage.setItem(city);
-console.log(searchHistory);
-var testObject = { 'one': 1, 'two': 2, 'three': 3 };
-
-// Put the object into storage
-localStorage.setItem('cityList', JSON.stringify(cityList));
-
-// Retrieve the object from storage
-var retrievedObject = localStorage.getItem('cityList');
-
-console.log('retrievedObject: ', JSON.parse(retrievedObject));
+localStorage.setItem("cityName", searchTerm.val());
+localStorage.getItem(searchTerm.val());
+console.log(localStorage.getItem("cityName"));
+var cityName = localStorage.getItem("cityName");
 });
-var cityList = {}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// //ALL BELOW IS CODE TO FORMAT TO FETCH API AND ETC
-// ////////////////////////////////////////////////////////////////
-// var format = $("#format");
-
-// submitBtn.on("click", function (event) {
-//     event.preventDefault();
-//     // alert("hello");
-//     // console.log(searchTerm.val());
-//     var userSearchTerm = searchTerm.val();
-//     var userFormat = format.val();
-//     document.location.href = "./page2.html?search=" + userSearchTerm + "&format=" + userFormat;
-// });
-
-
-// var mainEl = $("#main"); 
-// //^ showing results for chicago in a div above a h3 that says that
-
-
-// var urlData = document.location.search;
-// var mainEl = $("#main");
-// // console.log(urlData);
-
-// var searchTerm = urlData.split("&")[0].split("=")[1];
-// var format = urlData.split("&")[1].split("=")[1];
-
-// var apiUrl = "https://www.loc.gov/collections/?fo=json&title=chicago&original_format=map";
-
-// fetch(citySearch)
-//     .then(function (response) {
-//         return response.json();
-//     })
-//     .then(function (data) {
-//         console.log(data.results[0]);
-//         for (let i = 0; i < data.results.length; i++) {
-
-
-//             var articleEl = $("<article class='p-10'>");
-//             var titleEl = $("<h2>");
-//             var pEl = $("<p>");
-//             var descriptionEl = $("<p>");
-//             var moreBtn = $("<button>");
-
-//             titleEl.text(data.results[i].title);
-//             articleEl.append(titleEl);
-
-//             descriptionEl.text(data.results[i].description[0]);
-//             articleEl.append(descriptionEl);
-
-//             mainEl.append(articleEl);
-//         }
-//     });
-    
-// ////////////////////////////////////////////////////////////////
-// //ALL AB
