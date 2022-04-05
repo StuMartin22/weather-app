5d2c957b83aefb84d60068ed0c441e0646bcfc7d
StuMartin22 <stuart.martin@rocketmail.com>
2022-04-05 18:56:01 -0500

    created array tosave user input, put that into local storage by stringify, pulled from local storage and parsed then created a button element with parsed info

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,8 +5,8 @@ var submitBtn = $("#submit");
 
 
 submitBtn.on("click", function(event) {
-    event.preventDefault();
-    var newURL = citySearch.split("{city name}").join(searchTerm.val());
+    // event.preventDefault();
+    // var newURL = citySearch.split("{city name}").join(searchTerm.val());
     // console.log(newURL);
 // fetch(newURL)
 //     .then(function (response) {
@@ -15,17 +15,49 @@ submitBtn.on("click", function(event) {
 //     .then(function (data) {
 //         console.log(data.results)
 //     });
-localStorage.setItem("cityName", searchTerm.val());
+var savedUserInput = [];
+savedUserInput.push(searchTerm.val());
+searchTerm.text(" ");
+localStorage.setItem(savedUserInput, JSON.stringify(savedUserInput));
+var cityName = localStorage.getItem(savedUserInput);
+var historyContainer= $(".historyContainer");
+var searchHistory = $('<button>');
+historyContainer.append(searchHistory);
+JSON.parse(cityName);
+searchHistory.text(savedUserInput);
+});
+
+// localStorage.setItem("cityName", searchTerm.val());
 // localStorage.getItem(searchTerm.val()); this also pulls it
 // console.log(localStorage.getItem("cityName"));
-var historyContainer = $("#historyBtnList");
-var cityName = localStorage.getItem("cityName");
-console.log(cityName);
-var searchHistory = document.createElement('button')
-historyContainer.append(searchHistory);
-searchHistory.innerHTML(cityName);
-// const li = $('<li>').appendTo(historyContainer).text(cityName);
-// var itemITookOutOfStorage = $('<li>').text(cityName);
-// var itemITookOutOfStorage = document.createElement('li')
-// itemITookOutOfStorage.textContent = cityName;
-});
+// var cityName = localStorage.getItem("cityName");
+// console.log(cityName);
+// var historyContainer = $(".historyContainer");
+// var searchHistory = $('<button>');
+
+// searchHistory.text(value);
+// historyContainer.append(searchHistory);
+
+
+
+
+// var recentSearches = []; // create an empty javascript array
+
+// //this function is called using the search buttons "onclick"
+// function searchFunction(data) {
+    
+//     recentSearches.push($('#textboxSearch').val()); // This line puts the value from the text box in an array
+//     $('#textboxSearch').val(""); //  clear the text box after search
+//     $('#searchHistory').text(""); //clear the seach history window then repopulate with the new array
+    
+//     // the function below loops through the array and adds each item in the array
+//     // to the span element within the Search history arear
+//     $.each(recentSearches, function (index, value) {
+//         $('#searchHistory').append("<li class='historyItem'  onclick='addtotextbox("+index+")'>" + value + '</li>');
+//     });
+// }
+
+// function addtotextbox(id)
+// {
+// $('#textboxSearch').val(recentSearches[id]);
+// }
