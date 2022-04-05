dce876913002653e0af44cb5621dc891a5d74db5
StuMartin22 <stuart.martin@rocketmail.com>
2022-04-05 18:58:29 -0500

    removed some of the old commented out code references from JS

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -26,38 +26,3 @@ historyContainer.append(searchHistory);
 JSON.parse(cityName);
 searchHistory.text(savedUserInput);
 });
-
-// localStorage.setItem("cityName", searchTerm.val());
-// localStorage.getItem(searchTerm.val()); this also pulls it
-// console.log(localStorage.getItem("cityName"));
-// var cityName = localStorage.getItem("cityName");
-// console.log(cityName);
-// var historyContainer = $(".historyContainer");
-// var searchHistory = $('<button>');
-
-// searchHistory.text(value);
-// historyContainer.append(searchHistory);
-
-
-
-
-// var recentSearches = []; // create an empty javascript array
-
-// //this function is called using the search buttons "onclick"
-// function searchFunction(data) {
-    
-//     recentSearches.push($('#textboxSearch').val()); // This line puts the value from the text box in an array
-//     $('#textboxSearch').val(""); //  clear the text box after search
-//     $('#searchHistory').text(""); //clear the seach history window then repopulate with the new array
-    
-//     // the function below loops through the array and adds each item in the array
-//     // to the span element within the Search history arear
-//     $.each(recentSearches, function (index, value) {
-//         $('#searchHistory').append("<li class='historyItem'  onclick='addtotextbox("+index+")'>" + value + '</li>');
-//     });
-// }
-
-// function addtotextbox(id)
-// {
-// $('#textboxSearch').val(recentSearches[id]);
-// }
