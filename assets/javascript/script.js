597ea6603da608198ad5562423c2b57dfcf44f89
StuMartin22 <stuart.martin@rocketmail.com>
2022-04-06 21:27:21 -0500

    started working on 5 day variables. added some classes to elements and resized some. modified some minor css elements temporarily.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -13,7 +13,7 @@ var cI = $("#currentIcon");
 submitBtn.on("click", function(event) {
     event.preventDefault();
     var newURL = citySearch.split("{city name}").join(searchTerm.val());
-    console.log(newURL);
+    // console.log(newURL);
 
 var coords 
 
@@ -22,12 +22,11 @@ fetch (newURL)
         return response.json();
     })
     .then (function (data) {
-        // console.log(data[0].lat);
 coords = {
     lat: data[0].lat.toFixed(2),
     lon: data[0].lon.toFixed(2),
     };
-// console.log(coords)
+
 
 
 var secondUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + coords.lat + "&lon=" + coords.lon + "&exclude=minutely,hourly,alerts&limit=5&units=imperial&appid=7395305ec624eec55c8c9f460f751d9d"
@@ -37,16 +36,13 @@ fetch (secondUrl)
         return response.json();
     })
     .then (function(data) {
-        // console.log(data)
         var currentTemp = data.current.temp;
         var currentWind = data.current.wind_speed;
         var currentHumidity = data.current.humidity;
         var currentUvi = data.current.uvi;
         var currentIconCode = data.current.weather[0].icon;
         var currentIconUrl = "http://openweathermap.org/img/wn/" + currentIconCode + "@2x.png";
-        // console.log(currentIcon);
-        // var fiveDay = for (var i = 0; i < 6; i++) {
-        //     var day = [data[0].daily[i].temp, data[0].daily[i].wind_speed, data[0].daily[i].humidity]
+        var currentImageHolder = $("<img>");
     
         cT.append(currentTemp);
         cT.text("Temperature: " + currentTemp + " °F");
@@ -56,10 +52,23 @@ fetch (secondUrl)
         cH.text(" Humidity: " + currentHumidity + " %");
         cU.append(currentUvi);
         cU.text("UV Index: " + currentUvi);
-        var currentImageHolder = $("<img>");
         cI.append(currentImageHolder);
         currentImageHolder.attr('src', currentIconUrl);
         currentContainer.append(cT, cW, cH, cU, cI);
+
+
+        //fiveday data here:
+        // var fiveDay = for (var i = 0; i < 6; i++) {
+        // var fiveDayTemp = data.current.temp;
+        // var fiveDayWind = data.current.wind_speed;
+        // var fiveDayHumidity = data.current.humidity;
+        // var fiveDayUvi = data.current.uvi;
+        // var fiveDayIconCode = data.current.weather[0].icon;
+        // var fiveDayIconUrl = "http://openweathermap.org/img/wn/" + currentIconCode + "@2x.png";
+        // var fiveDayImageHolder = $("<img>");
+
+        // tempwindhumidityforecast
+
     });
 
 
